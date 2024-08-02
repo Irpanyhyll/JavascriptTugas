d2af1a0d82e9abbfdfcdd2eb7b3f11bbd9618507
Irpanyhyll <ifauzi781@gmail.com>
2024-08-02 13:48:42 +0700

    JavascriptTugas

---
+++ variable.js
@@ -0,0 +1,15 @@
+// const name = "Irpan";
+// name = "Eduwork";
+// console.log(name);
+
+//const student = {
+//    name: "Bima" ,
+//    class: 2
+//}
+
+//student.faculty = "Sainteks";
+//console.log(student);
+
+const students = ['Sandi', 'Agung', 'Anggi'];
+students.push('Eka');
+console.log(students);
